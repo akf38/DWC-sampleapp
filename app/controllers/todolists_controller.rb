def55a1694d621f141bfdb082ee3274bb52516e6
EC2 Default User <ec2-user@ip-172-31-28-163.ec2.internal>
2021-01-13 07:56:49 +0000

    Finish rubcop

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -8,25 +8,25 @@ class TodolistsController < ApplicationController
     list.save
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
@@ -34,6 +34,7 @@ class TodolistsController < ApplicationController
   end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
